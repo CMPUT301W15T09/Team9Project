fc167ca664b4259c08ebf97016648d6df9cd8c9b
Indragie Karunaratne <i@indragie.com>
2015-01-24 23:03:20 -0700

    Added EditingActivity abstract class
    Shows Cancel and Done buttons in the action bar.
    
    Images from http://developer.android.com/design/downloads/index.html

--- gen/com/indragie/cmput301as1/R.java
+++ gen/com/indragie/cmput301as1/R.java
@@ -22,11 +22,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_accept=0x7f020000;
+        public static final int ic_action_cancel=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_add_claim=0x7f080004;
-        public static final int claim_detail=0x7f080003;
+        public static final int action_add_claim=0x7f080006;
+        public static final int action_cancel=0x7f080003;
+        public static final int action_done=0x7f080004;
+        public static final int claim_detail=0x7f080005;
         public static final int claim_detail_container=0x7f080002;
         public static final int editText1=0x7f080000;
         public static final int editText2=0x7f080001;
@@ -34,7 +38,8 @@ public final class R {
     public static final class layout {
         public static final int activity_add_expense_claim=0x7f030000;
         public static final int activity_claim_detail=0x7f030001;
-        public static final int fragment_claim_detail=0x7f030002;
+        public static final int activity_editing_actionbar=0x7f030002;
+        public static final int fragment_claim_detail=0x7f030003;
     }
     public static final class menu {
         public static final int add_expense_claim=0x7f070000;
@@ -43,8 +48,10 @@ public final class R {
     public static final class string {
         public static final int action_add_claim=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int description=0x7f050006;
-        public static final int name=0x7f050005;
+        public static final int cancel_label=0x7f050007;
+        public static final int description_hint=0x7f050006;
+        public static final int done_label=0x7f050008;
+        public static final int name_hint=0x7f050005;
         public static final int no_expenses=0x7f050003;
         public static final int title_activity_add_expense_claim=0x7f050004;
         public static final int title_claim_detail=0x7f050001;
@@ -76,5 +83,10 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  From https://github.com/android/platform_packages_apps_calendar/blob/master/res/values/styles.xml 
+         */
+        public static final int EditCustomActionButton=0x7f060002;
+        public static final int EditCustomActionButtonImage=0x7f060003;
+        public static final int EditCustomActionButtonText=0x7f060004;
     }
 }
