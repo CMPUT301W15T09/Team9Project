7fdcabca9e302185bc2a1fe2489c69b2b47ba1d8
Indragie Karunaratne <i@indragie.com>
2015-01-25 19:39:37 -0700

    Show dates on detail view

--- src/com/indragie/cmput301as1/ExpenseClaimDetailActivity.java
+++ src/com/indragie/cmput301as1/ExpenseClaimDetailActivity.java
@@ -1,5 +1,7 @@
 package com.indragie.cmput301as1;
 
+import java.util.Date;
+import java.text.DateFormat;
 import java.util.ArrayList;
 
 import android.content.Intent;
@@ -9,18 +11,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ExpenseClaimDetailActivity extends ListActivity {
 	public static final String EXTRA_CLAIM = "com.indragie.cmput301as1.EXTRA_CLAIM";
+	private ExpenseClaim claim;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
-		ExpenseClaim claim = (ExpenseClaim)getIntent().getSerializableExtra(EXTRA_CLAIM);
+		claim = (ExpenseClaim)getIntent().getSerializableExtra(EXTRA_CLAIM);
 		setTitle(claim.getName());
-		
+		setupListHeaderView();
+		setListAdapter(new ExpenseClaimArrayAdapter(this, new ArrayList<ExpenseClaim>()));
+	}
+	
+	private void setupListHeaderView() {
 		ListView listView = getListView();
 		View headerView = getLayoutInflater().inflate(R.layout.activity_claim_detail_header, listView, false);
 		
@@ -32,8 +40,20 @@ public class ExpenseClaimDetailActivity extends ListActivity {
 		etDescription.setText(claim.getDescription());
 		etDescription.setEnabled(claim.isEditable());
 		
+		DateFormat formatter = DateFormat.getDateInstance(DateFormat.LONG);
+		Date startDate = claim.getStartDate();
+		if (startDate != null) {
+			TextView tvStartDate = (TextView)headerView.findViewById(R.id.tv_start_date);
+			tvStartDate.setText(formatter.format(startDate));
+		}
+		
+		Date endDate = claim.getEndDate();
+		if (endDate != null) {
+			TextView tvEndDate = (TextView)headerView.findViewById(R.id.tv_end_date);
+			tvEndDate.setText(formatter.format(endDate));
+		}
+		
 		listView.addHeaderView(headerView);
-		setListAdapter(new ExpenseClaimArrayAdapter(this, new ArrayList<ExpenseClaim>()));
 	}
 
 	@Override
