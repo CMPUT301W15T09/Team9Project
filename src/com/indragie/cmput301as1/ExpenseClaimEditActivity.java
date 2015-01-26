060d775cf7bb4b83cfe9854c691acc8213932cb7
Indragie Karunaratne <i@indragie.com>
2015-01-25 20:38:58 -0700

    Change ExpenseClaimEditActivity superclass back to ListActivity
    EditingActivity wouldn’t allow for an overflow button

--- src/com/indragie/cmput301as1/ExpenseClaimEditActivity.java
+++ src/com/indragie/cmput301as1/ExpenseClaimEditActivity.java
@@ -4,49 +4,50 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.util.ArrayList;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ExpenseClaimEditActivity extends EditingActivity {
+public class ExpenseClaimEditActivity extends ListActivity {
 	//================================================================================
 	// Constants
 	//================================================================================
 	public static final String EXTRA_CLAIM = "com.indragie.cmput301as1.EXTRA_CLAIM";
 	public static final String EXTRA_CLAIM_POSITION = "com.indragie.cmput301as1.EXTRA_CLAIM_POSITION";
-	
+
 	//================================================================================
 	// Properties
 	//================================================================================
-	
+
 	private ExpenseClaim claim;
 	private int claimPosition;
-	private ListView listView;
 
 	//================================================================================
 	// Activity Callbacks
 	//================================================================================
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
+
 		Intent intent = getIntent();
 		claim = (ExpenseClaim)intent.getSerializableExtra(EXTRA_CLAIM);
 		claimPosition = intent.getIntExtra(EXTRA_CLAIM_POSITION, 0);
+		setTitle(claim.getName());
 
-		setContentView(R.layout.activity_claim_edit);
-		listView = (ListView)findViewById(R.id.list);
+		ListView listView = getListView();
 		listView.addHeaderView(getListHeaderView());
 		listView.setAdapter(new ExpenseClaimArrayAdapter(this, new ArrayList<ExpenseClaim>()));
 	}
 
 	private View getListHeaderView() {
-		View headerView = getLayoutInflater().inflate(R.layout.activity_claim_edit_header, listView, false);
+		View headerView = getLayoutInflater().inflate(R.layout.activity_claim_edit_header, getListView(), false);
 
 		EditText etName = (EditText)headerView.findViewById(R.id.et_name);
 		etName.setText(claim.getName());
@@ -83,10 +84,20 @@ public class ExpenseClaimEditActivity extends EditingActivity {
 		return headerView;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == android.R.id.home) {
+			navigateUpTo(new Intent(this, ExpenseClaimListActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	//================================================================================
 	// EditingActivity
 	//================================================================================
-	
+
 	protected void onDone() {
 		Intent intent = new Intent();
 		intent.putExtra(EXTRA_CLAIM, claim);
