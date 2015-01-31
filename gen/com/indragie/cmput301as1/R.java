c32ce26971297424b0e0be0a840fc0304be78b37
Indragie Karunaratne <i@indragie.com>
2015-01-30 22:21:16 -0700

    Implement ExpenseItemArrayAdaptor

--- gen/com/indragie/cmput301as1/R.java
+++ gen/com/indragie/cmput301as1/R.java
@@ -28,11 +28,12 @@ public final class R {
     public static final class drawable {
         public static final int bg_rounded_blue=0x7f020000;
         public static final int bg_rounded_green=0x7f020001;
-        public static final int bg_rounded_red=0x7f020002;
-        public static final int bg_rounded_yellow=0x7f020003;
-        public static final int ic_action_accept=0x7f020004;
-        public static final int ic_action_cancel=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
+        public static final int bg_rounded_grey=0x7f020002;
+        public static final int bg_rounded_red=0x7f020003;
+        public static final int bg_rounded_yellow=0x7f020004;
+        public static final int ic_action_accept=0x7f020005;
+        public static final int ic_action_cancel=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
         public static final int action_add_claim=0x7f090013;
