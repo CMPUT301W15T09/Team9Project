9b8be510952267935a52ddd66195305f414ea411
OriginalFool <nmrcarroll@gmail.com>
2015-03-10 19:55:37 -0600

    User class created and first run user selection.# Please enter the commit message for your changes. Lines starting

--- Team9Project/gen/com/indragie/cmput301as1/R.java
+++ Team9Project/gen/com/indragie/cmput301as1/R.java
@@ -38,14 +38,14 @@ public final class R {
     }
     public static final class id {
         public static final int action_add_claim=0x7f090016;
-        public static final int action_add_item=0x7f090011;
+        public static final int action_add_item=0x7f090014;
         public static final int action_cancel=0x7f090008;
         public static final int action_delete=0x7f090010;
         public static final int action_done=0x7f090009;
-        public static final int action_email=0x7f090012;
-        public static final int action_mark_approved=0x7f090015;
-        public static final int action_mark_returned=0x7f090014;
-        public static final int action_mark_submitted=0x7f090013;
+        public static final int action_email=0x7f090011;
+        public static final int action_mark_approved=0x7f090013;
+        public static final int action_mark_returned=0x7f090012;
+        public static final int action_mark_submitted=0x7f090015;
         public static final int et_amount=0x7f09000c;
         public static final int et_date=0x7f09000d;
         public static final int et_description=0x7f090003;
@@ -71,7 +71,8 @@ public final class R {
     public static final class menu {
         public static final int contextual_delete=0x7f080000;
         public static final int expense_claim_edit=0x7f080001;
-        public static final int expense_claim_list=0x7f080002;
+        public static final int expense_claim_edit_claimant=0x7f080002;
+        public static final int expense_claim_list=0x7f080003;
     }
     public static final class string {
         /**  ExpenseClaimListActivity 
