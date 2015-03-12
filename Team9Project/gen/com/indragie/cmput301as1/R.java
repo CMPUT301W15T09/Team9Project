1a97f8551685fd520c8bf87ddbf709fb911e417a
OriginalFool <nmrcarroll@gmail.com>
2015-03-12 14:14:04 -0600

    Modifying claim uml

--- Team9Project/gen/com/indragie/cmput301as1/R.java
+++ Team9Project/gen/com/indragie/cmput301as1/R.java
@@ -37,29 +37,31 @@ public final class R {
         public static final int ic_launcher=0x7f020008;
     }
     public static final class id {
-        public static final int action_add_claim=0x7f090016;
-        public static final int action_add_item=0x7f090014;
-        public static final int action_cancel=0x7f090008;
-        public static final int action_delete=0x7f090010;
-        public static final int action_done=0x7f090009;
-        public static final int action_email=0x7f090011;
-        public static final int action_mark_approved=0x7f090013;
-        public static final int action_mark_returned=0x7f090012;
-        public static final int action_mark_submitted=0x7f090015;
-        public static final int et_amount=0x7f09000c;
-        public static final int et_date=0x7f09000d;
+        public static final int action_add_claim=0x7f090018;
+        public static final int action_add_item=0x7f090016;
+        public static final int action_cancel=0x7f09000a;
+        public static final int action_delete=0x7f090012;
+        public static final int action_done=0x7f09000b;
+        public static final int action_email=0x7f090013;
+        public static final int action_mark_approved=0x7f090015;
+        public static final int action_mark_returned=0x7f090014;
+        public static final int action_mark_submitted=0x7f090017;
+        public static final int et_amount=0x7f09000e;
+        public static final int et_date=0x7f09000f;
         public static final int et_description=0x7f090003;
         public static final int et_end_date=0x7f090005;
         public static final int et_name=0x7f090002;
         public static final int et_start_date=0x7f090004;
-        public static final int sp_category=0x7f09000a;
-        public static final int sp_currency=0x7f09000b;
+        public static final int sp_category=0x7f09000c;
+        public static final int sp_currency=0x7f09000d;
         public static final int tv_amounts=0x7f090001;
-        public static final int tv_date=0x7f09000e;
+        public static final int tv_approver=0x7f090009;
+        public static final int tv_date=0x7f090010;
         public static final int tv_end_label=0x7f090007;
         public static final int tv_name=0x7f090000;
         public static final int tv_start_label=0x7f090006;
-        public static final int tv_status=0x7f09000f;
+        public static final int tv_status=0x7f090011;
+        public static final int tv_user=0x7f090008;
     }
     public static final class layout {
         public static final int activity_claim_footer=0x7f030000;
@@ -91,12 +93,14 @@ public final class R {
         /**  General 
          */
         public static final int app_name=0x7f060000;
+        public static final int approver=0x7f06001c;
         /**  EditingActivity 
          */
         public static final int cancel_label=0x7f06000d;
         /**  ExpenseItem 
          */
         public static final int category=0x7f06000a;
+        public static final int comments=0x7f06001d;
         public static final int date=0x7f06000c;
         public static final int dates=0x7f060003;
         /**  ExpenseClaim 
@@ -116,6 +120,7 @@ public final class R {
         public static final int status_returned=0x7f060009;
         public static final int status_submitted=0x7f060007;
         public static final int total_label=0x7f06001a;
+        public static final int user=0x7f06001b;
     }
     public static final class style {
         /** 
