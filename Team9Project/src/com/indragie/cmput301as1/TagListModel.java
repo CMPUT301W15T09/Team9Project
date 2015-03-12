ea8e038c28ddc68f0669663fe4aea5903326567d
mrthegreensky <azhong@ualberta.ca>
2015-03-12 10:52:55 -0600

    Added Manage tags activity, has button that can be shown

--- Team9Project/src/com/indragie/cmput301as1/TagListModel.java
+++ Team9Project/src/com/indragie/cmput301as1/TagListModel.java
@@ -1,11 +1,46 @@
 package com.indragie.cmput301as1;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
+import android.content.Context;
+
 public class TagListModel extends ListModel<Tag>{
+	
+	private String fileName;
+	private Context context;
+
+
+	public TagListModel(String fileName, Context context) {
+		this.fileName = fileName;
+		this.context = context;
+		this.list = load();
+	}
+	
 	
 	public void setTags(ArrayList<Tag> tags) {
 		this.list = tags;
 	}
+	
+	public ArrayList<Tag> getTags() {
+		return this.list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ArrayList<Tag> load() {
+		try {
+			FileInputStream fis = context.openFileInput(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Object obj = ois.readObject();
+			ois.close();
+			fis.close();
+			return (ArrayList<Tag>)obj;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ArrayList<Tag>();
+		}
+	}
+	
 
 }
