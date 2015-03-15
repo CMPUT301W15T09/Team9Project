f35216e6a4d6b50fcf46f6dd4926d377b54f52c7
Indragie Karunaratne <i@indragie.com>
2015-03-14 23:11:25 -0600

    Implement ExpenseClaimDetailModel

--- Team9Project/src/com/indragie/cmput301as1/Destination.java
+++ Team9Project/src/com/indragie/cmput301as1/Destination.java
@@ -84,4 +84,40 @@ public class Destination implements Serializable {
 	public void setTravelReason(String travelReason) {
 		this.travelReason = travelReason;
 	}
+	
+	//================================================================================
+	// Object
+	//================================================================================
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((travelReason == null) ? 0 : travelReason.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Destination other = (Destination) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (travelReason == null) {
+			if (other.travelReason != null)
+				return false;
+		} else if (!travelReason.equals(other.travelReason))
+			return false;
+		return true;
+	}
 } 
--- Team9Project/src/com/indragie/cmput301as1/ExpenseItem.java
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseItem.java
@@ -150,4 +150,59 @@ public class ExpenseItem implements Serializable, Comparable<ExpenseItem> {
 	public int compareTo(ExpenseItem item) {
 		return this.getDate().compareTo(item.getDate());
 	}
+	
+	//================================================================================
+	// Object
+	//================================================================================
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ExpenseItem other = (ExpenseItem) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
