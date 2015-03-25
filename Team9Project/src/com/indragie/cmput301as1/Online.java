4e8abaa48c5779dd035e6244e2a7f24b8ed4f295
jimmyho899 <jph@ualberta.ca>
2015-03-24 21:35:32 -0600

    Need implement user online information

--- Team9Project/src/com/indragie/cmput301as1/Online.java
+++ Team9Project/src/com/indragie/cmput301as1/Online.java
@@ -1,5 +1,6 @@
 package com.indragie.cmput301as1;
 
+// might have to move this class into users. 
 public class Online {
 	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t09/";
 	private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t09/";
@@ -11,4 +12,6 @@ public class Online {
 	public String getSearchUrl() {
 		return SEARCH_URL;
 	}
+	
+	// MAY NEED TO ADD LISTENER CLASS
 }
--- Team9Project/src/com/indragie/cmput301as1/OnlineController.java
+++ Team9Project/src/com/indragie/cmput301as1/OnlineController.java
@@ -1,18 +1,60 @@
 package com.indragie.cmput301as1;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+import ca.ualberta.ssrg.movies.es.Movies;
 
 import com.google.gson.Gson;
 
 public class OnlineController {
 	private Gson gson = new Gson();
+	private static final String TAG = "OnlineController";
+	private User users;
+	
+	public OnlineController(User users) {
+		super();
+		this.users = users;
+	}
 
 	public void addOnline(/*add user info*/) {
 		HttpClient httpClient = new DefaultHttpClient();
+		
+		// need try and except block here
+		try {
+			HttpPost addRequest = new HttpPost(/* the user information*/);
+
+			StringEntity stringEntity = new StringEntity(gson.toJson(/*user*/));
+			addRequest.setEntity(stringEntity);
+			addRequest.setHeader("Accept", "application/json");
+
+			HttpResponse response = httpClient.execute(addRequest);
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void deleteOnline(/*User info*/) {
 		HttpClient httpClient = new DefaultHttpClient();
+		// try except block NEEDS FIXING
+		try {
+			HttpDelete deleteRequest = new HttpDelete(/* the user information */);
+			deleteRequest.setHeader("Accept", "application/json");
+
+			HttpResponse response = httpClient.execute(deleteRequest);
+			String status = response.getStatusLine().toString();
+			Log.i(TAG, status);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
