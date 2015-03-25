e34a2eebc47678842ccbbb530cf5bf1a3506c256
jimmyho899 <jph@ualberta.ca>
2015-03-24 21:10:01 -0600

    Created branch for elastic search. Not sure what exactly will be done online.

---
+++ Team9Project/src/com/indragie/cmput301as1/Online.java
@@ -0,0 +1,14 @@
+package com.indragie.cmput301as1;
+
+public class Online {
+	private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t09/";
+	private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t09/";
+	
+	public String getResourceUrl() {
+		return RESOURCE_URL;
+	}
+
+	public String getSearchUrl() {
+		return SEARCH_URL;
+	}
+}
---
+++ Team9Project/src/com/indragie/cmput301as1/OnlineController.java
@@ -0,0 +1,18 @@
+package com.indragie.cmput301as1;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+
+public class OnlineController {
+	private Gson gson = new Gson();
+
+	public void addOnline(/*add user info*/) {
+		HttpClient httpClient = new DefaultHttpClient();
+	}
+	
+	public void deleteOnline(/*User info*/) {
+		HttpClient httpClient = new DefaultHttpClient();
+	}
+}
