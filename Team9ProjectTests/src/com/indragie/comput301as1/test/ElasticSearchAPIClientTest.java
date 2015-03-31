5b8366ce2764c6246af7d75fbe15248dbfc08b4f
Indragie Karunaratne <i@indragie.com>
2015-03-30 19:49:36 -0600

    Start implementing tests for ElasticSearchAPIClient

---
+++ Team9ProjectTests/src/com/indragie/comput301as1/test/ElasticSearchAPIClientTest.java
@@ -0,0 +1,113 @@
+/* 
+ * Copyright (C) 2015 Jimmy Ho
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.indragie.comput301as1.test;
+
+import java.io.IOException;
+
+import com.indragie.cmput301as1.ElasticSearchAPIClient;
+import com.indragie.cmput301as1.ElasticSearchAPIClient.RequestFailedException;
+import com.indragie.cmput301as1.ElasticSearchDocument;
+import com.indragie.cmput301as1.ElasticSearchDocumentID;
+import com.squareup.okhttp.mockwebserver.*;
+
+import junit.framework.TestCase;
+
+public class ElasticSearchAPIClientTest extends TestCase 
+{
+	private ElasticSearchAPIClient client;
+	private MockWebServer server;
+	
+	@Override
+	protected void setUp() throws Exception {
+		server = new MockWebServer();
+		server.start();
+		client = new ElasticSearchAPIClient(server.getUrl("/"));
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		server.shutdown();
+		super.tearDown();
+	}
+	
+	public void testAdd() throws InterruptedException, RequestFailedException, IOException {
+		MockResponse response = new MockResponse()
+	    	.addHeader("Content-Type", "application/json; charset=utf-8")
+	    	.addHeader("Cache-Control", "no-cache")
+	    	.setBody("{\"_index\":\"test\",\"_type\":\"doc\",\"_id\":\"100\",\"_version\":1,\"created\":true}");
+		server.enqueue(response);
+		
+		TestDocument document = new TestDocument("Indragie Karunaratne", 3);
+		TestDocument resultDocument = client.add(document).execute();
+		assertEquals(document, resultDocument);
+		
+		RecordedRequest request = server.takeRequest();
+		assertEquals("/test/doc/100", request.getPath());
+		assertEquals("POST", request.getMethod());
+	}
+	
+	private class TestDocument implements ElasticSearchDocument {
+		private String name;
+		private int year;
+		
+		TestDocument(String name, int year) {
+			this.name = name;
+			this.year = year;
+		}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + year;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			TestDocument other = (TestDocument) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (year != other.year)
+				return false;
+			return true;
+		}
+		
+		private ElasticSearchAPIClientTest getOuterType() {
+			return ElasticSearchAPIClientTest.this;
+		}
+		
+		@Override
+		public ElasticSearchDocumentID getDocumentID() {
+			return new ElasticSearchDocumentID("test", "doc", "100");
+		}
+	}
+}
