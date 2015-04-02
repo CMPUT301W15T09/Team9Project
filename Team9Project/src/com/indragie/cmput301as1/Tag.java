90852d5c07bab04280ed33af54853f1af7e92573
mrthegreensky <azhong@ualberta.ca>
2015-04-01 21:17:07 -0600

    Added overrides for equals and hashcode for tags.

--- Team9Project/src/com/indragie/cmput301as1/Tag.java
+++ Team9Project/src/com/indragie/cmput301as1/Tag.java
@@ -67,5 +67,41 @@ public class Tag implements Serializable, Comparable<Tag>{
 	public int compareTo(Tag tag) {
 		return this.name.compareTo(tag.getName());
 	}
+	
+	//================================================================================
+	// Object
+	//================================================================================
+
+
+	@Override
+	public int hashCode()
+	{
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 }
