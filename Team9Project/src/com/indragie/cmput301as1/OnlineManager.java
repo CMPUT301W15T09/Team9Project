30f413656f7fc84719012c99c97080454ae3e920
jimmyho899 <jph@ualberta.ca>
2015-04-03 17:30:15 -0600

    Fixed comments and indentation.

--- Team9Project/src/com/indragie/cmput301as1/OnlineManager.java
+++ Team9Project/src/com/indragie/cmput301as1/OnlineManager.java
@@ -34,71 +34,84 @@ import android.util.Log;
 public class OnlineManager<T extends ElasticSearchDocument> extends BroadcastReceiver{
 
 	private LinkedList<ElasticSearchAPIClient.APICall<T>> stack = new LinkedList<ElasticSearchAPIClient.APICall<T>>(); 
-	
+
+	/**
+	 * adds a call to the stack. Then attempts to execute it.
+	 * @param APICall<T>
+	 */
 	public void enqueueCall(ElasticSearchAPIClient.APICall<T> call) {
 		stack.add(call);
-		checkForNetwork();
+		attemptNextAPICall();
 	}
-	
+
 	private Context context;
 
-	public void checkForNetwork() {
+	/**
+	 * checks for APICalls in the stack and for network connection then executes the APICall
+	 */
+	private void attemptNextAPICall() {
 		// if there is something inside of the stack, do this, else do nothing
 		if (stack.size() > 0) {	
-			
-			if (isNetworkAvailable(context) == true) {
+
+			if (isNetworkAvailable(context)) {
 				// we are connected so then we want to go through with our stack calls
-				
-				// removes and returns the first item from the linked list
 				APICall<T> call = stack.getFirst();
+
 				// now do something with the call
 				call.enqueue(new ElasticSearchAPIClient.APICallback<T>() {
 					@Override
 					public void onFailure(Request request, Response response, IOException e) {
-						// on failure you want to check the network
-						if (isNetworkAvailable(context)==false) {
-							// if network is down, wait for connection
-							// do nothing as this will get called again once connection is established
+						// if response is not null then it means that server returned a bad HTTP status code
+						// otherwise the response is null which means connection has been lost
+						if (response!=null) {
+							stack.removeFirst();
+							try {
+								throw new Exception("Bad HTTP status code");
+							} catch (Exception e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
 						} 
 					}
-	
+
 					@Override
 					public void onSuccess(Response response, T document) {
-					    // on success you want to recall if there is still something in the stack
+						// on success you want to recall if there is still something in the stack
 						// remove the call from the stack
 						stack.removeFirst();
 						Log.d("test", "worked");
 						if (stack.size()>0) {
-							checkForNetwork();
+							attemptNextAPICall();
 						}
 					}
 				});
 			}
 		}
 	}
-	
+
 	// http://stackoverflow.com/questions/12157130/internet-listener-android-example
 	// we come into this code once we get connection / lose connection
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.d("app","Network connectivity change");
-	     if(intent.getExtras()!=null) {
-	        NetworkInfo ni=(NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
-	        if(ni!=null && ni.getState()==NetworkInfo.State.CONNECTED) {
-	            Log.i("app","Network "+ni.getTypeName()+" connected");
-	            checkForNetwork();
-	        }
-	     }
-	     if(intent.getExtras().getBoolean(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
-	            Log.d("app","There's no network connectivity");
-	     }
+		if(intent.getExtras()!=null) {
+			@SuppressWarnings("deprecation")
+			NetworkInfo ni=(NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
+			if(ni!=null && ni.getState()==NetworkInfo.State.CONNECTED) {
+				Log.i("app","Network "+ni.getTypeName()+" connected");
+				attemptNextAPICall();
+			}
+		}
+		if(intent.getExtras().getBoolean(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
+			Log.d("app","There's no network connectivity");
+		}
 	}
-	
+
 	// http://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
 	public boolean isNetworkAvailable(Context context) {
-	    ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 
 }
