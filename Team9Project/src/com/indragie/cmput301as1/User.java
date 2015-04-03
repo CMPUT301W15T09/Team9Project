9a852b19613edc022ea08910479b8ec2c00da855
Indragie Karunaratne <karunara@ualberta.ca>
2015-04-03 15:15:14 -0600

    Implement hashCode() and equals() for User

--- Team9Project/src/com/indragie/cmput301as1/User.java
+++ Team9Project/src/com/indragie/cmput301as1/User.java
@@ -76,4 +76,40 @@ public class User implements Serializable, ElasticSearchDocument {
 	public ElasticSearchDocumentID getDocumentID() {
 		return documentID;
 	}
+	
+	//================================================================================
+	// Object
+	//================================================================================
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((documentID == null) ? 0 : documentID.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (documentID == null) {
+			if (other.documentID != null)
+				return false;
+		} else if (!documentID.equals(other.documentID))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
