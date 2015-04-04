ed2f24f1f7f7e4c7f1104f9377a87ca02905f5c6
jimmyho899 <jph@ualberta.ca>
2015-04-03 19:53:40 -0600

    Added delete in list activity.

--- Team9Project/src/com/indragie/cmput301as1/ExpenseClaimListActivity.java
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseClaimListActivity.java
@@ -299,6 +299,10 @@ public class ExpenseClaimListActivity extends ListActivity implements TypedObser
 		openDialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
+				OnlineManager<ExpenseClaim> onlineManager = new OnlineManager<ExpenseClaim>(ExpenseClaimListActivity.this);
+				ElasticSearchAPIClient apiClient = new ElasticSearchAPIClient(ElasticSearchConfiguration.getBaseURL()); 
+				onlineManager.enqueueCall(apiClient.delete(listModel.getItems().get(longPressedItemIndex)));
+				
 				listModel.remove(longPressedItemIndex);
 			}
 		});
