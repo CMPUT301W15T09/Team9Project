c7525e1b9f55c9edb1c61b3e701e53ddd2973641
Indragie Karunaratne <i@indragie.com>
2015-04-05 20:08:20 -0600

    Add stub classes for ExpenseItemJSONSerializer/Deserializer

---
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseItemJSONSerializer.java
@@ -0,0 +1,38 @@
+/* 
+ * Copyright (C) 2015 Indragie Karunaratne
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.indragie.cmput301as1;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * Serializes an {@link ExpenseItem} to JSON.
+ */
+public class ExpenseItemJSONSerializer implements JsonSerializer<ExpenseItem> {
+
+	/* (non-Javadoc)
+	 * @see com.google.gson.JsonSerializer#serialize(java.lang.Object, java.lang.reflect.Type, com.google.gson.JsonSerializationContext)
+	 */
+	@Override
+	public JsonElement serialize(ExpenseItem src, Type typeOfSrc, JsonSerializationContext context) {
+		return null;
+	}
+}
