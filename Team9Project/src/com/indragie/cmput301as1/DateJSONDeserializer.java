2a2229020e2d0b91eb5cdf5f75d0bc246368cc6d
Indragie Karunaratne <i@indragie.com>
2015-04-05 22:44:11 -0600

    Implement serializers, tests pass

--- Team9Project/src/com/indragie/cmput301as1/DateJSONDeserializer.java
+++ Team9Project/src/com/indragie/cmput301as1/DateJSONDeserializer.java
@@ -24,6 +24,7 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 
 /**
  * Deserializes {@link Date} objects from JSON.
@@ -35,7 +36,12 @@ public class DateJSONDeserializer implements JsonDeserializer<Date> {
 	 */
 	@Override
 	public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-		// TODO Auto-generated method stub
+		if (json.isJsonPrimitive()) {
+			JsonPrimitive primitive = json.getAsJsonPrimitive();
+			if (primitive.isNumber()) {
+				return new Date(primitive.getAsLong());
+			}
+		}
 		return null;
 	}
 }
