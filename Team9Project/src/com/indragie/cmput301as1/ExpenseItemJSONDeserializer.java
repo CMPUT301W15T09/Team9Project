9f7078e3a79652975d49c69ce6362cf595ced3eb
Indragie Karunaratne <karunara@ualberta.ca>
2015-04-05 20:08:20 -0600

    Add stub classes for ExpenseItemJSONSerializer/Deserializer

---
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseItemJSONDeserializer.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (C) 2015 Indragie Karunaratne
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.indragie.cmput301as1;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+/**
+ * Deserializes an {@link ExpenseItem} from JSON.
+ */
+public class ExpenseItemJSONDeserializer implements JsonDeserializer<ExpenseItem> {
+
+	/* (non-Javadoc)
+	 * @see com.google.gson.JsonDeserializer#deserialize(com.google.gson.JsonElement, java.lang.reflect.Type, com.google.gson.JsonDeserializationContext)
+	 */
+	@Override
+	public ExpenseItem deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
