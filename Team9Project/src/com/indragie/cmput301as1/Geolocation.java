4b0efcd3a352b43e669980d5aee48e417dff5d6c
Indragie Karunaratne <karunara@ualberta.ca>
2015-04-06 17:07:43 -0600

    Add name and place to Geolocation

--- Team9Project/src/com/indragie/cmput301as1/Geolocation.java
+++ Team9Project/src/com/indragie/cmput301as1/Geolocation.java
@@ -19,6 +19,7 @@ package com.indragie.cmput301as1;
 
 import java.io.Serializable;
 
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -26,11 +27,12 @@ import com.google.android.gms.maps.model.LatLng;
  * and longitude coordinates.
  */
 public class Geolocation implements Serializable {
-	private static final long serialVersionUID = -2951789773586259550L;
+	private static final long serialVersionUID = 1223468022963281449L;
+	
 	//================================================================================
 	// Properties
 	//================================================================================
-
+	
 	/**
 	 * Latitude coordinate.
 	 */
@@ -40,6 +42,16 @@ public class Geolocation implements Serializable {
 	 * Longitude coordinate.
 	 */
 	private double longitude;
+	
+	/**
+	 * The name of the location.
+	 */
+	private String name;
+	
+	/**
+	 * The address of the location.
+	 */
+	private String address;
 	
 	//================================================================================
 	// Constructors
@@ -64,6 +76,16 @@ public class Geolocation implements Serializable {
 		this.longitude = latlng.longitude;
 	}
 	
+	/**
+	 * Creates a new instance of {@link Geolocation}
+	 * @param place The {@link Place} instance to get information from.
+	 */
+	public Geolocation(Place place) {
+		this(place.getLatLng());
+		this.name = place.getName().toString();
+		this.address = place.getAddress().toString();
+	}
+	
 	//================================================================================
 	// Accessors
 	//================================================================================
@@ -76,10 +98,56 @@ public class Geolocation implements Serializable {
 	}
 	
 	/**
+	 * Sets the latitude coordinate.
+	 * @param latitude The latitude coordinate.
+	 */
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	/**
 	 * @return The longitude coordinate.
 	 */
 	public double getLongitude() {
 		return longitude;
+	}
+	
+	/**
+	 * Sets the longitude coordinate.
+	 * @param longitude The longitude coordinate.
+	 */
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * @return The name of the location.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the name of the location.
+	 * @param name The name of the location.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return The address of the location.
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * Sets the address of the location.
+	 * @param address The address.
+	 */
+	public void setAddress(String address) {
+		this.address = address;
 	}
 	
 	//================================================================================
@@ -90,11 +158,13 @@ public class Geolocation implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
 		long temp;
 		temp = Double.doubleToLongBits(latitude);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		temp = Double.doubleToLongBits(longitude);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -107,11 +177,21 @@ public class Geolocation implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Geolocation other = (Geolocation) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
 		if (Double.doubleToLongBits(latitude) != Double
 				.doubleToLongBits(other.latitude))
 			return false;
 		if (Double.doubleToLongBits(longitude) != Double
 				.doubleToLongBits(other.longitude))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
 			return false;
 		return true;
 	}
