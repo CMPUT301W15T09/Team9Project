f9fed23e33b07db402584ff9b1b12a6b81ba75eb
Indragie Karunaratne <karunara@ualberta.ca>
2015-04-05 21:14:03 -0600

    Implemented ExpenseItemJSONDeserializer
    Tests don't pass yet.

--- Team9Project/src/com/indragie/cmput301as1/ExpenseItemJSONDeserializer.java
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseItemJSONDeserializer.java
@@ -17,24 +17,116 @@
 
 package com.indragie.cmput301as1;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Date;
+
+import org.joda.money.Money;
+
+import android.net.Uri;
+import android.util.Base64;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 
 /**
  * Deserializes an {@link ExpenseItem} from JSON.
  */
 public class ExpenseItemJSONDeserializer implements JsonDeserializer<ExpenseItem> {
+	/**
+	 * Controller used to generate receipt URIs.
+	 */
+	private ExpenseItemReceiptController receiptController;
 
+	/**
+	 * Creates an instance of {@link ExpenseItemJSONDeserializer}
+	 * @param receiptController Controller used to generate receipt URIs.
+	 */
+	public ExpenseItemJSONDeserializer(ExpenseItemReceiptController receiptController) {
+		this.receiptController = receiptController;
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.google.gson.JsonDeserializer#deserialize(com.google.gson.JsonElement, java.lang.reflect.Type, com.google.gson.JsonDeserializationContext)
 	 */
 	@Override
 	public ExpenseItem deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-		// TODO Auto-generated method stub
+		JsonObject obj = json.getAsJsonObject();
+		if (obj == null) return null;
+		
+		String name = getStringIfPossible(obj.get("name"));
+		String description = getStringIfPossible(obj.get("description"));
+		String category = getStringIfPossible(obj.get("category"));
+		boolean incomplete = getBooleanIfPossible(obj.get("incomplete"));
+		JsonObject amountObject = getJsonObjectIfPossible(obj.get("amount"));
+		JsonObject dateObject = getJsonObjectIfPossible(obj.get("date"));
+		String receiptBase64Str = getStringIfPossible(obj.get("receipt_base64"));
+		
+		Money amount = null;
+		if (amountObject != null) {
+			amount = context.deserialize(amountObject, Money.class);
+		}
+		
+		Date date = null;
+		if (dateObject != null) {
+			date = context.deserialize(dateObject, Money.class);
+		}
+		
+		ExpenseItem item = new ExpenseItem(name, description, category, amount, date);
+		item.setIncomplete(incomplete);
+		
+		if (receiptBase64Str != null) {
+			Uri receiptUri = receiptController.constructNewReceiptImageUri();
+			byte buffer[] = Base64.decode(receiptBase64Str, Base64.DEFAULT);
+			try {
+				FileOutputStream fos = new FileOutputStream(receiptUri.getPath(), false);
+				fos.write(buffer);
+				fos.flush();
+				fos.close();
+				item.setReceiptUri(receiptUri);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return item;
+	}
+	
+	private static String getStringIfPossible(JsonElement element) {
+		if (element == null) return null;
+		
+		if (element.isJsonPrimitive()) {
+			JsonPrimitive primitive = element.getAsJsonPrimitive();
+			if (primitive.isString()) {
+				return primitive.getAsString();
+			}
+		}
+		return null;
+	}
+	
+	private static boolean getBooleanIfPossible(JsonElement element) {
+		if (element == null) return false;
+		
+		if (element.isJsonPrimitive()) {
+			JsonPrimitive primitive = element.getAsJsonPrimitive();
+			if (primitive.isBoolean()) {
+				return primitive.getAsBoolean();
+			}
+		}
+		return false;
+	}
+	
+	private static JsonObject getJsonObjectIfPossible(JsonElement element) {
+		if (element == null) return null;
+		
+		if (element.isJsonObject()) {
+			return element.getAsJsonObject();
+		}
 		return null;
 	}
 }
