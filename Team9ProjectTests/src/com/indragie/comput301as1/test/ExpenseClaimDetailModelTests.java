306554587aef079907b2298cb5b82b2600d3df92
mrthegreensky <azhong@ualberta.ca>
2015-04-07 14:52:56 -0600

    Fix tests

--- Team9ProjectTests/src/com/indragie/comput301as1/test/ExpenseClaimDetailModelTests.java
+++ Team9ProjectTests/src/com/indragie/comput301as1/test/ExpenseClaimDetailModelTests.java
@@ -108,7 +108,10 @@ public class ExpenseClaimDetailModelTests extends TestCase {
 		model.addComment(comment1);
 		assertEquals(comment1, model.getComments().get(0));
 		
-		model.addComment(comment2);
-		assertEquals(comment2, model.getComments().get(1));
+		model.addComment(comment1);
+		assertEquals(comment2, model.getComments().get(0));
+		
+		model.addComment(comment1);
+		assertEquals(comment1, model.getComments().get(1));
 	}
 }
