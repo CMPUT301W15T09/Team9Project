1c2be51265658e1d599c90f316524a7042d453cb
Indragie Karunaratne <i@indragie.com>
2015-04-06 23:15:22 -0600

    Tabs in ExpenseClaimListActivity

--- Team9Project/src/com/indragie/cmput301as1/ExpenseClaimListActivity.java
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseClaimListActivity.java
@@ -17,30 +17,44 @@
 
 package com.indragie.cmput301as1;
 
-import android.app.ListActivity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 /**
  * An activity that presents a list of expense claims.
  */
-public class ExpenseClaimListActivity extends ListActivity {
+public class ExpenseClaimListActivity extends FragmentActivity {
 	//================================================================================
 	// Constants
 	//================================================================================
-	
+
 	private static final int USER_SETTINGS_REQUEST = 6;
-	
+
 	//================================================================================
 	// Properties
 	//================================================================================
-	
+
 	/**
 	 * Manages the user and associated preferences.
 	 */
 	private UserManager userManager;
+
+	/**
+	 * Adapter used to show fragments in the {@link ViewPager}
+	 */
+	private ExpenseClaimPagerAdapter pagerAdapter;
+
+	/**
+	 * Pager view for the owned/reviewal claims tabs.
+	 */
+	private ViewPager pager;
 
 	//================================================================================
 	// Activity Callbacks
@@ -49,15 +63,18 @@ public class ExpenseClaimListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		setContentView(R.layout.activity_expense_claim_list);
+		pager = (ViewPager)findViewById(R.id.pager);
+
 		userManager = new UserManager(this);
 		if (userManager.getActiveUser() == null) {
 			startUserSettingsActivity();
 		} else {
-			loadData();
+			setupFragments();
 		}
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
@@ -69,7 +86,7 @@ public class ExpenseClaimListActivity extends ListActivity {
 			break;
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.expense_claim_list_activity, menu);
@@ -86,25 +103,62 @@ public class ExpenseClaimListActivity extends ListActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 	/**
-	 * Loads the expense claim data.
+	 * Set up fragments to display expense claim data.
 	 */
-	private void loadData() {
-		Session session = new Session(this, userManager.getActiveUser());
+	private void setupFragments() {
+		User user = userManager.getActiveUser();
+		Session session = new Session(this, user);
 		Session.setSharedSession(session);
+
+		pagerAdapter = new ExpenseClaimPagerAdapter(this, getSupportFragmentManager(), user);
+		pager.setAdapter(pagerAdapter);
+
+		// From http://developer.android.com/training/implementing-navigation/lateral.html
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+			public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+				pager.setCurrentItem(tab.getPosition());
+			}
+			public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {}
+			public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
+		};
+
+		pager.setOnPageChangeListener(
+			new ViewPager.SimpleOnPageChangeListener() {
+				@Override
+				public void onPageSelected(int position) {
+					actionBar.setSelectedNavigationItem(position);
+				}
+			});
+
+		Tab ownedTab = actionBar.newTab()
+			.setText(R.string.tab_owned)
+			.setTabListener(tabListener);
+		Tab reviewalTab = actionBar.newTab()
+			.setText(R.string.tab_reviewal)
+			.setTabListener(tabListener);
+
+		actionBar.addTab(ownedTab);
+		actionBar.addTab(reviewalTab);
 	}
-	
+
 	/**
 	 * Sets the active user of the application.
 	 * @param data The intent to get the user from.
 	 */
 	private void onUserSettingsResult(Intent data) {
 		User user = (User)data.getSerializableExtra(UserSettingsActivity.EXTRA_USER);
+		boolean noExistingUser = (userManager.getActiveUser() == null);
 		userManager.setActiveUser(user);
-		loadData();
+		if (noExistingUser) {
+			setupFragments();
+		}
 	}
-	
+
 	/**
 	 * Starts the {@link UserSettingsActivity}
 	 */
