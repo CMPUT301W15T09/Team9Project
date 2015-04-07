e0906f9114701fbf7f4b9e362889dc739e5753b6
mrthegreensky <azhong@ualberta.ca>
2015-04-06 22:15:40 -0600

    Added javadocs and licences

--- Team9Project/src/com/indragie/cmput301as1/DestinationAddActivity.java
+++ Team9Project/src/com/indragie/cmput301as1/DestinationAddActivity.java
@@ -1,3 +1,20 @@
+/* 
+ * Copyright (C) 2015 Andrew Zhong
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.indragie.cmput301as1;
 
 import com.google.android.gms.location.places.Place;
@@ -10,28 +27,73 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+/**
+ * Activity that presents a user interface for entering information to 
+ * add a destination to a claim.
+ */
 public class DestinationAddActivity extends PlacePickerParentActivity {
+	//================================================================================
+	// Constants
+	//================================================================================
 	
+	/**
+	 * Intent key for the {@link Destination} object.
+	 */
 	public static final String EXTRA_DESTINATION = "com.indragie.cmput301as1.EXTRA_DESTINATION";
+	
+	/**
+	 * Intent key for the {@link User} object.
+	 */
 	public static final String EXTRA_USER = "com.indragie.cmput301as1.EXTRA_USER";
 	
+	//================================================================================
+	// Properties
+	//================================================================================
+	
+	/**
+	 * Field for name of the destination.
+	 */
 	protected EditText nameField;
+	
+	/**
+	 * Field for reason of travel.
+	 */
 	protected EditText reasonField;
+	
+	/**
+	 * Field for adding a location to the destination.
+	 */
 	protected EditText addLocationField;
 	
+	/**
+	 * Geolocation of the location for the destination.
+	 */
 	protected Geolocation location;
 	
-	protected boolean noLocationAdded = true;
-	private User user;
+	//================================================================================
+	// Activity Callbacks
+	//================================================================================
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setUpFields();
 		setUpListener();
-		user = (User)getIntent().getSerializableExtra(EXTRA_USER);
+		
+		User user = (User)getIntent().getSerializableExtra(EXTRA_USER);
+		location = user.getLocation();
+		
+		if (location == null) {
+			System.out.println("was null");
+			location = new Geolocation(0, 0);
+		}
+		addLocationField.setText(location.getName() + "\n" + location.getAddress());
 	}
 
+	/**
+	 * Sets up the text fields for the activity.
+	 */
 	protected void setUpFields() {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_destination_add);
@@ -39,8 +101,12 @@ public class DestinationAddActivity extends PlacePickerParentActivity {
 		nameField = (EditText)findViewById(R.id.et_name);
 		reasonField = (EditText)findViewById(R.id.et_travel_reason);
 		addLocationField = (EditText)findViewById(R.id.et_add_location);
+		
 	}
 	
+	/**
+	 * Sets up the PlacePickedListener for the activity.
+	 */
 	protected void setUpListener() {
 		final OnPlacePickedListener listener = new OnPlacePickedListener() {
 			
@@ -56,7 +122,6 @@ public class DestinationAddActivity extends PlacePickerParentActivity {
 				addLocationField.clearFocus();
 				
 				location = new Geolocation(place);
-				noLocationAdded = false;
 			}
 		};
 		
@@ -85,9 +150,6 @@ public class DestinationAddActivity extends PlacePickerParentActivity {
 			String name = nameField.getText().toString();
 			String reason = reasonField.getText().toString();
 			
-			if(noLocationAdded) {
-				location = user.getLocation();
-			}
 			Destination destination = new Destination(name, reason, location);
 			System.out.println("Name after saving in destination: " +location.getName());
 			System.out.println("Address after saving in destination: " + location.getAddress());
