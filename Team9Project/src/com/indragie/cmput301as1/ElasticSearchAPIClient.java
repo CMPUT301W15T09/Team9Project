0a9dc347d63c8f9bf4bf54f670ab413985118e73
Indragie Karunaratne <i@indragie.com>
2015-04-07 00:37:19 -0600

    Make sure all the request codes are different

--- Team9Project/src/com/indragie/cmput301as1/ElasticSearchAPIClient.java
+++ Team9Project/src/com/indragie/cmput301as1/ElasticSearchAPIClient.java
@@ -30,7 +30,6 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
