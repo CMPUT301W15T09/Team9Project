cc0b4d1a5f9f3619bdbaa13af3b50873e791b434
Brandon <bjw1@ualberta.ca>
2015-04-06 22:15:46 -0600

    Locations now saveable in expense items

--- Team9Project/src/com/indragie/cmput301as1/ExpenseItemEditActivity.java
+++ Team9Project/src/com/indragie/cmput301as1/ExpenseItemEditActivity.java
@@ -29,89 +29,104 @@ import android.view.Menu;
  * Activity for editing the attributes of an expense item.
  */
 public class ExpenseItemEditActivity extends ExpenseItemAddActivity {
-	
-	//================================================================================
+
+	// ================================================================================
 	// Constants
-	//================================================================================
-	
+	// ================================================================================
+
 	/**
 	 * Intent key for the position of the {@link ExpenseItem} object.
 	 */
 	public static final String EXTRA_EXPENSE_ITEM_POSITION = "com.indragie.cmput301as1.EXPENSE_ITEM_POSITION";
-	
+
 	/**
 	 * Intent key for editing an {@link ExpenseItem} object.
 	 */
 	public static final String EXTRA_EXPENSE_ITEM_EDITABLE = "com.indragie.cmput301as1.EXPENSE_ITEM_EDITABLE";
-	
-	//================================================================================
+
+	// ================================================================================
 	// Properties
-	//================================================================================
-	
+	// ================================================================================
+
 	/**
 	 * Flag for setting the activity fields as editable or not.
 	 */
 	private static boolean editable;
-	
-	//================================================================================
+
+	// ================================================================================
 	// Activity Callbacks
-	//================================================================================
+	// ================================================================================
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// The superclass shows a custom action bar with Cancel and Done buttons by default
+		// The superclass shows a custom action bar with Cancel and Done buttons
+		// by default
 		// and that needs to be disabled to show the Up button.
-		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
+		getActionBar().setDisplayOptions(
+				ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_HOME_AS_UP
+						| ActionBar.DISPLAY_SHOW_TITLE);
 
 		Intent intent = getIntent();
-		ExpenseItem item = (ExpenseItem)getIntent().getSerializableExtra(EXTRA_EXPENSE_ITEM);
+		ExpenseItem item = (ExpenseItem) getIntent().getSerializableExtra(
+				EXTRA_EXPENSE_ITEM);
 		editable = intent.getBooleanExtra(EXTRA_EXPENSE_ITEM_EDITABLE, false);
+		expenseLocation = item.getLocation();
 		receiptFileUri = item.getReceiptUri();
 		incomplete = item.isIncomplete();
-		
+
 		setTitle(item.getName());
 		setupFields(item, editable);
 	}
-	
+
 	/**
 	 * Sets up the fields of the expense item to edit.
-	 * @param item The expense item.
-	 * @param editable Boolean if fields are editable. 
+	 * 
+	 * @param item
+	 *            The expense item.
+	 * @param editable
+	 *            Boolean if fields are editable.
 	 */
 	private void setupFields(ExpenseItem item, Boolean editable) {
 		Money amount = item.getAmount();
 
 		nameField.setText(item.getName());
 		nameField.setEnabled(editable);
-		
+
 		descriptionField.setText(item.getDescription());
 		descriptionField.setEnabled(editable);
-		
-		SpinnerUtils.setSelectedItem(currencySpinner, amount.getCurrencyUnit().toString());
+
+		if (expenseLocation != null) {
+			locationField.setText(item.getLocation().toString());
+			locationField.setEnabled(editable);
+		}
+
+		SpinnerUtils.setSelectedItem(currencySpinner, amount.getCurrencyUnit()
+				.toString());
 		currencySpinner.setEnabled(editable);
-		
+
 		amountField.setText(amount.getAmount().toString());
 		amountField.setEnabled(editable);
-		
+
 		dateField.setDate(item.getDate());
 		dateField.setEnabled(editable);
-		
+
 		SpinnerUtils.setSelectedItem(categorySpinner, item.getCategory());
 		categorySpinner.setEnabled(editable);
-		
+
 		receiptButton.setClickable(editable);
 		if (receiptFileUri != null) {
-			Drawable receiptPic = Drawable.createFromPath(receiptFileUri.getPath());
+			Drawable receiptPic = Drawable.createFromPath(receiptFileUri
+					.getPath());
 			receiptButton.setImageDrawable(receiptPic);
 		}
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.expense_item_edit, menu);
-		menu.findItem(R.id.action_set_incomplete).setChecked(incomplete).setEnabled(editable); 
+		menu.findItem(R.id.action_set_incomplete).setChecked(incomplete)
+				.setEnabled(editable);
 		return true;
 	}
 
@@ -122,14 +137,15 @@ public class ExpenseItemEditActivity extends ExpenseItemAddActivity {
 		onDone();
 	}
 
-	//================================================================================
+	// ================================================================================
 	// ExpenseItemAddActivity Overrides
-	//================================================================================
+	// ================================================================================
 
 	@Override
-	protected Intent getResultIntent()  {
+	protected Intent getResultIntent() {
 		Intent intent = super.getResultIntent();
-		intent.putExtra(EXTRA_EXPENSE_ITEM_POSITION, getIntent().getIntExtra(EXTRA_EXPENSE_ITEM_POSITION, -1));
+		intent.putExtra(EXTRA_EXPENSE_ITEM_POSITION,
+				getIntent().getIntExtra(EXTRA_EXPENSE_ITEM_POSITION, -1));
 		return intent;
 	}
 }
